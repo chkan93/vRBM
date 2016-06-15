84b91a81809d5f2c3ebb1ad6f653c5563bf8aca6
Xinyu Zhang <xiz368@ucsd.edu>
2016-06-15 11:39:37 -0700

    add one iadder

--- test_bench/sigmoid_tb.v
+++ test_bench/sigmoid_tb.v
@@ -13,7 +13,7 @@ module test_sigmoid;
     #10 sum= 2047;
     for(i=1;i<4096;i=i+1)begin
      #10 sum=sum-1;
-     $display("i = %-d, (%0d/256 = %f)  output is: %0d",sum+1,sum+1, $bitstoreal((sum+1)/256), s);
+     #10 $display("i = %-d, (%0d/256 = %f)  output is: %0d",sum+1,sum+1, $bitstoreal((sum+1)/256), s);
     end
   end
 endmodule
