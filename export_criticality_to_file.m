1cb72f360c7c05ec8c9af4d4d20dc7b17cbd51de
xy0 <xiz368@eng.ucsd.edu>
2016-07-06 11:01:58 -0700

    export criticality

--- export_criticality_to_file.m
+++ export_criticality_to_file.m
@@ -1,7 +1,7 @@
-N = 80;
+N = 300;
 load ./mat_data/criticality.mat
 
 for i = 1:length(critorder)
-    export_data(strcat('generated_data/', 'critorder.',num2str(i),'.',num2str(N),'.txt'), ...
+    export_data(strcat('generated_data/order/', 'critorder.',num2str(i),'.',num2str(N),'.txt'), ...
         generate_criticality(critorder{i}, N),  0);
 end
