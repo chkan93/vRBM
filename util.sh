e2fe08cf58e241a3a4691866a95ddc96257da76d
xy0 <xiz368@eng.ucsd.edu>
2016-07-07 14:25:52 -0700

    fix random_string

--- util.sh
+++ util.sh
@@ -3,7 +3,8 @@
 source "setup.sh"
 
 function random_string(){
-    echo $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+    # echo $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+    echo $(pwgen 8 1)
 }
 
 
