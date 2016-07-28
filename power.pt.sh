ade2c688386896d5cffa74bdb087bf77bbf474f7
Sayen Xinyu Zhang <sayenzhang@gmail.com>
2016-07-27 19:56:03 -0700

    Update power.pt.sh

--- power.pt.sh
+++ power.pt.sh
@@ -2,7 +2,11 @@
 
 source "setup.sh"
 
-
+# this one is kind of complex
+# 1. generate gate level saif from dc, no compile ultra, use -propogated and proper change_names
+# 2. write sdc from dc shell
+# 3. in primetime read sdc, foreach to read saif
+# 4. analyze report_power result. 
 
 function pt_measure_power {
     echo "TO DO"
