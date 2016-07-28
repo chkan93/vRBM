445d8e465825a0c9be4ebf8db08ab59d561f1e3e
g1 <g1@dfm.ucsd.edu>
2016-07-27 19:29:30 -0700

    modify dc_python_path and dc_path

--- setup.power.sh
+++ setup.power.sh
@@ -5,8 +5,8 @@ DC_SOURCE="/home/g1/vRBM"
 DC_DEST="/home/g1/SAIFS"
 DC_ADDERS=(ZHU4 ETAIIM) #
 DC_SOURCE_ROOT='/home/g1/psimulate'
-DC_PYTHON_PATH=''
-DC_PATH=''
+DC_PYTHON_PATH='/home/g1/python27'
+DC_PATH='/home/tool/synopsys/DesignCompiler/K-2015.06-SP4/bin/'
 
 
 
