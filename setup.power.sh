cbf00bd97ca62daf8555f8fb8573221b121090b6
Xinyu Zhang <sayenzhang@gmail.com>
2016-09-12 18:02:12 -0700

    Update setup.power.sh

--- setup.power.sh
+++ setup.power.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
 
-DC_SOURCE="/home/g1/vRBM"
-DC_DEST="/home/g1/SAIFS"
-DC_ADDERS=(ZHU4 ETAIIM) #
-DC_SOURCE_ROOT='/home/g1/psimulate'
-DC_PYTHON_PATH='/home/g1/python27'
+DC_SOURCE="/home/g1/RBM_Workspace/vRBM"
+DC_DEST="/home/g1/RBM_Workspace/saif_output"
+DC_ADDERS=(4B) #
+DC_SOURCE_ROOT='/home/g1/RBM_Workspace/parallel_script'
+
+DC_PYTHON_PATH='/usr/local/bin'
 DC_PATH='/home/tool/synopsys/DesignCompiler/K-2015.06-SP4/bin/'
 
 
-
-PT_SOURCE="/home/linux/ieng6/oce/6i/xiz368/vRBM"
-PT_DEST="/home/linux/ieng6/oce/6i/xiz368/SAIFS"
-PT_ADDERS=(ZHU4 ETAIIM 8A 4B 6B ZHU) #
-PT_SOURCE_ROOT='/home/linux/ieng6/oce/6i/xiz368/psimulate'
